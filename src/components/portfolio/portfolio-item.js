1693ffc233e89356b831344f87964692aa67539d
effervescent000 <kislio01@gmail.com>
2022-02-02 12:53:06 -0500

    portfolio styling

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -4,12 +4,31 @@ const PortfolioItem = (props) => {
     const [portfolioItemClass, setPortfolioItemClass] = useState("");
 
     const { id, description, thumb_image_url, logo_url } = props.item;
+
+    const handleMouseEnter = () => {
+        setPortfolioItemClass("image-blur");
+    };
+
+    const handleMouseLeave = () => {
+        setPortfolioItemClass("");
+    };
+
     return (
-        <div className="portfolio-item-wrapper">
+        <div
+            className="portfolio-item-wrapper"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
             <div
                 className={`portfolio-img-background ${portfolioItemClass}`}
                 style={{ backgroundImage: `url(${thumb_image_url})` }}
             />
+            <div className="img-text-wrapper">
+                <div className="logo-wrapper">
+                    <img src={logo_url} alt="logo" />
+                </div>
+                <div className="subtitle">{description}</div>
+            </div>
         </div>
     );
 };
